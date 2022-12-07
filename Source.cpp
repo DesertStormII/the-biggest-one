bf683df86408883c4f22b4494c40a51f4e7a7e94
DesertStormII <119744433+DesertStormII@users.noreply.github.com>
2022-12-07 22:49:32 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int f, g;//обмен значениями
+	cout << "value exchange" << endl;
+	cout << "f=";
+	cin >> f;
+	cout << "g=";
+	cin >> g;
+	cout << "f=" << f;
+	cout << "      ";
+	cout << "g=" << g;
+
+
+	return 0;
+}
